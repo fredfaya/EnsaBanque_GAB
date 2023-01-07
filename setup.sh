316663e406253a3f39165c0ed36cf9b7e5dad75f
fredfaya <edyfoottv@gmail.com>
2023-01-08 00:48:00 +0100

    update setup.sh and requirements

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 mkdir -p ~/.streamlit/
 
-# shellcheck disable=SC2028
 echo "\
 [server]\n\
-headless = $PORT\n\
+headless = true\n\
+port = $PORT\n\
 enableCORS = false\n\
 \n\
 " > ~/.streamlit/config.toml
